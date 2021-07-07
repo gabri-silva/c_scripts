0fb84d887591a6f28e14b50c7dc5be73e730cbbf
dom1nick <60849061+gabri-silva@users.noreply.github.com>
2021-07-07 15:12:42 -0300

    Update dns_resolver.c

--- dns_resolver.c
+++ dns_resolver.c
@@ -3,19 +3,33 @@
 #include <arpa/inet.h>
 
 int main(int argc, char *argv[]){
-        //program use. example
-        if( argc<=1){
-                printf("USE: ./program [host]\n");
+
+        int iteration;
+        char line;
+
+        FILE *file;
+        file = fopen(argv[2], "r");
+        if(file == NULL){
+                printf("[!] ERROR FILE [!]");
+        }
+        if(argc<=2){
+                printf("|||||||||||||||||||||||||||||||||||||||");
+                printf("|||||||||||||DNS RESOLVER||||||||||||||");
+                printf("|||||||||||||||||1.1|||||||||||||||||||");
+                printf("||use: ./program [host] [wordlist.txt]|");
+                printf("||||||||||Z2FicmllbCBhbG1laWRh|||||||||");
+                printf("|||||||||||||||||||||||||||||||||||||||");
                 return 0;
-        //case correct argc
         }else{
-                struct hostent *target = gethostbyname(argv[1]);
-                if(target == NULL){
-                        printf("[!] ERROR [!]");
-                }else{
-                //decode inet_ntoa for ascii data encode
-                //struct in address
-                printf("IP: %s\n",inet_ntoa(*((struct in_addr *)target->h_addr)));
+                for(iteration=1;iteration<=1024;iteration++){
+                        struct hostent *target gethostbyname(argv[1]);
+                        if(*target == NULL){
+                                printf("[!] HOST OFFLINE [!]");
+                                return 0;
+                        }else{
+                        printf("HOST ONLINE: %s",inet_ntoa(*((struct in_addr *)target->h_addr)));
+                        }
                 }
         }
 }
+
