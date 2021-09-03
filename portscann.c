0da5ea370b22017d16b233099b8b9bafa4035d42
dom1nick <60849061+gabri-silva@users.noreply.github.com>
2021-09-03 13:16:47 -0300

    Update portscann.c

--- portscann.c
+++ portscann.c
@@ -3,6 +3,7 @@
 #include <netdb.h>
 
 int main(int argc, char *argv[]){
+        //set variables type
         int mysocket;
         int conect;
 
@@ -11,7 +12,7 @@ int main(int argc, char *argv[]){
         int final = 65535;
         char *destiny;
         destiny = argv[1];
-
+        //scruct module construction
         struct sockaddr_in target;
 
         for(port=init;port<=final;port++){
@@ -19,8 +20,9 @@ int main(int argc, char *argv[]){
                 target.sin_family = AF_INET;
                 target.sin_port = htons(port);
                 target.sin_addr.s_addr = inet_addr(destiny);
-
+                //connect socket in port and target for condition validate host
                 conect = connect(mysocket, (struct sockaddr *)&target, sizeof target);
+                //condition for connection == 0
                 if(conect == 0){
                         printf("Port: %d - status [+]OPEN\n",port);
                         close(mysocket);
